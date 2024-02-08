bd4277a2ca71d9600816e8f9ff67d673d780e178
Mia Ekheizer <miaekh12@gmail.com>
2024-02-08 17:00:07 +0200

    finished design of first part, started RestaurantImpl

---
+++ Solution/RestaurantImpl.java
@@ -0,0 +1,13 @@
+package Solution;
+
+import java.util.Set;
+
+public class RestaurantImpl {
+    int id;
+    String name;
+    int distFromTech;
+    Set<String> menu;
+    Map<HungryStudent, int> studentRateMap;
+    int numOfRates;
+    int sumOfRates;
+}
